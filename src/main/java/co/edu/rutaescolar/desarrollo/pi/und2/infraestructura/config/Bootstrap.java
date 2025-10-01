d99df902736a79eeabcd3960931576a7a0d79836
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2025-09-30 20:04:19 -0500

    feat(infra): Bootstrap de arranque; cablea repos JDBC + caso de uso + CLI

--- src/main/java/co/edu/rutaescolar/desarrollo/pi/und2/infraestructura/config/Bootstrap.java
+++ src/main/java/co/edu/rutaescolar/desarrollo/pi/und2/infraestructura/config/Bootstrap.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package co.edu.rutaescolar.desarrollo.pi.und2.infraestructura.config;
 
-/**
- *
- * @author ASUS
- */
-public class Bootstrap {
-    
+import co.edu.rutaescolar.desarrollo.pi.und2.aplicacion.AsignarBusUseCase;
+import co.edu.rutaescolar.desarrollo.pi.und2.aplicacion.puertos.in.AsignarBusIn;
+import co.edu.rutaescolar.desarrollo.pi.und2.aplicacion.servicios.AsignarBusService;
+import co.edu.rutaescolar.desarrollo.pi.und2.dominio.servicios.AsignadorDeRecursos;
+import co.edu.rutaescolar.desarrollo.pi.und2.infraestructura.out.jdbc.*;
+import co.edu.rutaescolar.desarrollo.pi.und2.infraestructura.in.cli.CliAsignarBus;
+
+public final class Bootstrap {
+
+    public static void main(String[] args) {
+        var repoBus = new RepoBusJdbc();
+        var repoCon = new RepoConductorJdbc();
+        var asignador = new AsignadorDeRecursos();
+        var useCase = new AsignarBusUseCase(repoBus, repoCon, asignador);
+        AsignarBusIn api = new AsignarBusService(useCase);
+        new CliAsignarBus(api).run();
+    }
 }

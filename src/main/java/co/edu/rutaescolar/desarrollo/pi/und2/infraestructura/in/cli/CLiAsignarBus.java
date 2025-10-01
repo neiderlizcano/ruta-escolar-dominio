ede7b4670ca82eccedb20c420179724093396aa4
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2025-09-30 19:55:30 -0500

    feat(infra): adaptador de entrada CLI para asignación

--- src/main/java/co/edu/rutaescolar/desarrollo/pi/und2/infraestructura/in/cli/CLiAsignarBus.java
+++ src/main/java/co/edu/rutaescolar/desarrollo/pi/und2/infraestructura/in/cli/CLiAsignarBus.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package co.edu.rutaescolar.desarrollo.pi.und2.infraestructura.in.cli;
 
-/**
- *
- * @author ASUS
- */
-public class CLiAsignarBus {
-    
+import java.util.Scanner;
+import co.edu.rutaescolar.desarrollo.pi.und2.aplicacion.dto.*;
+import co.edu.rutaescolar.desarrollo.pi.und2.aplicacion.puertos.in.AsignarBusIn;
+
+public final class CliAsignarBus {
+
+    private final AsignarBusIn api;
+
+    public CliAsignarBus(AsignarBusIn api) {
+        this.api = api;
+    }
+
+    public void run() {
+        var sc = new Scanner(System.in);
+        System.out.print("RutaId: ");
+        var rutaId = sc.nextLine();
+        System.out.print("Demanda mínima: ");
+        var demanda = Integer.parseInt(sc.nextLine());
+        var resp = api.run(new AsignacionRequestDTO(rutaId, demanda));
+        System.out.println("OK -> Ruta=" + resp.rutaId() + " Bus=" + resp.busId() + " Conductor=" + resp.conductorId());
+    }
 }
